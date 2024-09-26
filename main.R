eee8127e4b30d02ab993614cf686a2e90266760a
ekoma <ekomarovaite@gmail.com>
2024-09-26 20:50:26 +0300

    ADD: loading packages and data.

--- main.R
+++ main.R
@@ -1,2 +1,21 @@
-library(data.table)
+# Remove everything from environment
+rm(list = ls())
 
+# Package name
+package.names <- c("data.table")
+
+# Loading packages
+for (pkg_name in package.names) {
+  if (!require(pkg_name, character.only = TRUE)) {
+    install.packages(pkg_name)
+    library(pkg_name, character.only = TRUE)
+  } else {
+    library(pkg_name, character.only = TRUE)
+  }
+}
+
+rm(pkg_name, package.names)
+
+# Loading data
+expenditures.data <- read.csv("Data/Consumption_expenditures.csv")
+individual.data <- read.csv("Data/Individual_data.csv")
