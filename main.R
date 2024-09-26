4a73fefc62d349a3a76900217298810489d34f4c
ekoma <ekomarovaite@gmail.com>
2024-09-26 21:44:35 +0300

    ADD: checking for NA values and household id duplicates.

--- main.R
+++ main.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 
 # Package name
-package.names <- c("data.table")
+package.names <- c("data.table", "dplyr")
 
 # Loading packages
 for (pkg_name in package.names) {
@@ -17,5 +17,28 @@ for (pkg_name in package.names) {
 rm(pkg_name, package.names)
 
 # Loading data
-expenditures.data <- read.csv("Data/Consumption_expenditures.csv")
-individual.data <- read.csv("Data/Individual_data.csv")
+expenditures.data <- data.table(read.csv("Data/Consumption_expenditures.csv"))
+individual.data <- data.table(read.csv("Data/Individual_data.csv"))
+#-------------------------------------------------------------------------------
+# Exploratory data analysis for expenditures data
+
+# Columns to leave (household identification id and only large groups of products are left)
+col.names <- c("hh_ident", colnames(expenditures.data)[nchar(colnames(expenditures.data)) == 4])
+expenditures.data <- expenditures.data[, ..col.names]
+
+# 1)
+# Checking for duplicates of household id
+dupl <- expenditures.data[, .N, by = hh_ident] %>%
+  .[N > 1, ]
+dupl # No duplicates (good)
+
+# 2)
+# Checking for NA values
+na.count <- expenditures.data[, lapply(.SD, function(x) sum(is.na(x)))] # No NA values are observed (good)
+
+# 3)
+
+
+
+ 
+
